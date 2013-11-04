fefab9779ddbe04057f995e34ba4a011f934b2df
Neal Patel <nealp9084@gmail.com>
2013-11-03 19:42:56 -0500

    more validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ActiveRecord::Base
-  belongs_to :goodie
   belongs_to :user
+  belongs_to :goodie
+
+  validates_presence_of :content
+  validates :content, length: { minimum: 3 }
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,8 @@
 class Order < ActiveRecord::Base
   belongs_to :user
   belongs_to :goodie
+
   validates_uniqueness_of :charge_token
+  validates_presence_of :charge_token, :destination
+  validates :destination, length: { minimum: 5 }
 end
