5c81bdc53ceebcd514782b9863dde3914326d9e7
Neal Patel <nealp9084@gmail.com>
2013-11-03 21:16:35 -0500

    grr

--- app/mailers/users_mailer.rb
+++ app/mailers/users_mailer.rb
@@ -6,7 +6,6 @@ class UsersMailer < ActionMailer::Base
 
   def welcome_email(user)
     @user = user
-    @url = url_for(:login, host: 'www.epicdomain.name')
     mail(to: @user.email, subject: 'Welcome to CakeSenders!')
   end
 end
