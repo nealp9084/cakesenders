a9e33ee6a263d7c65a02f7529f2e753e89c042bf
Neal Patel <nealp9084@gmail.com>
2013-12-01 17:40:03 -0500

    added actionmailer defaults to the test environment

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -26,6 +26,11 @@ Cakesenders::Application.configure do
   # Disable request forgery protection in test environment.
   config.action_controller.allow_forgery_protection = false
 
+  # dummy ActionMailer configuration
+  config.domain_name = 'localhost'
+  config.action_mailer.default_options = { from: "themuffinman@#{config.domain_name}" }
+  config.action_mailer.default_url_options = { host: config.domain_name, port: 3000 }
+
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
